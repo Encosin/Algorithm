bc4a64ff5025c998f36f97b5805f9bb9aee9f81a
Byeonghun-Choi <cbh3612@naver.com>
2022-05-10 10:23:52 +0900

    search

--- 2CT/Search.py
+++ 2CT/Search.py
@@ -139,3 +139,65 @@ input_data = sys.stdin.readline().rstrip()
 # 입력받은 문자열 그대로 출력 
 print(input_data)
 """
+
+
+
+# 실전문제 : 부품 찾기
+'''동빈이네 전자매장에는 부품이 N개 있다. 각 부품은 정수 형태의 고유한 번호가 있다. 어느날 손님이 M개 종류의 부품을 대량으로 구매하겠다며 당일 날 견적서를 요청했다.
+동빈이는 때를 놓치지 않고 손님이 문의한 부품 M개 종류를 모두 확인해서 견적서를 작성해야 한다. 이때 가게 안에 부품이 모두 있는지 확인하는 프로그램을 작성해보자.
+Ex) N=5 [8, 3, 7, 9, 2]  
+N은 가게의 부품의 수, 옆에 list는 가게에 있는 부품의 번호
+    M=3 [5, 7, 9] 
+M은 손님이 찾는 부품의 개수, 옆 list는 필요한 부품의 번호
+가게의 list 안에 손님이 요청한 list가 포함된다면 => Yes를 출력, 포함되지 않는다면 No를 출력
+'''
+
+'''해설
+1) 이진탐색 알고리즘 
+: 먼저 N개의 부품을 번호를 기준으로 정렬, 그 이후에 M개의 찾고자 하는 부품이 매장에 존재하는지 검사
+이렇게하면 최악의 경우에도 시간 복잡도 : O(M*logN) 의 연산이 필요하다. 최대 2만번의 연산
+하지만 N개의 부품을 정렬하기 위해서는 O(NxlogN) 이론적으로 약 2000만, 더 많은 연산이 필요하다.
+그래서 최종 시간복잡도는 O((M + N) x logN)'''
+
+# p.198 7-5.py 이진탐색 답안 예시
+def binary_search(array, target, start, end):
+    while start <- end:
+        mid = (start + end) // 2
+        
+        # 찾은 경우 중간점 인덱스 반환 
+        if array[mid] == target:
+            return mid 
+        
+        # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+        elif array[mid] > target:
+            end = mid - 1
+            
+        # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인 
+        else:
+            start = mid + 1
+            
+    return None
+
+# N(가게의 부품 개수) 입력 
+n = int(input())
+
+# 가게에 있는 전체 부품 번호를 공백으로 구분하여 입력
+array = list(map(int, input().split()))
+array.sort()  # 이진탐색을 수행하기 위해 사전에 정렬 수행
+
+# M(손님이 확인 요청한 부품 개수) 입력 
+m = int(input())
+
+# 손님이 확인 요청한 전체 부품 번호를 공백으로 구분하여 입력
+x = list(map(int, input().split()))
+
+# 손님이 확인 요청한 부품 번호를 하나씩 확인 
+for i in x :
+    # 해당 부품이 존재하는지 확인 
+    result = binary_search(array, i, 0, n-1)
+    if result != None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ') 
+        
+
